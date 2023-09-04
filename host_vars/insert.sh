497c82d7b49d29e32f98c9cf1c2d07f42935f81a
Juraj Dobrovodsky <xdobrovodsky.juraj@gmail.com>
2023-09-04 12:05:57 +0000

    do sshd configu to neprida medzeru pri AllowUsers - chyba!

--- host_vars/insert.sh
+++ host_vars/insert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-text_to_insert="apache_package_name: apache2
-apache_service: apache2
-php_package_name: libapache2-mod-php"
+text_to_insert="ssh_users: kub simone"
+text_to_insert1="ssh_template_files: sshd_config_ubuntu.j2"
 
-for server_ip in 192.168.26.142 192.168.26.143 192.168.26.144 192.168.26.145 192.168.26.146
+for server_ip in 192.168.26.142 192.168.26.143 192.168.26.144 192.168.26.145
 do
-  echo "$text_to_insert" > "$server_ip.yml"
+  echo "$text_to_insert" >> "$server_ip.yml"
+  echo "$text_to_insert1" >> "$server_ip.yml"
 done
 
